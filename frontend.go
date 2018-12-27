82a87b0259b3073b1e2ba46f05e5d31ae7fe4161
Vova Tkach <vladimirok5959@gmail.com>
2018-12-27 18:14:06 +0200

    Front-end templates

--- frontend.go
+++ frontend.go
@@ -1,9 +1,58 @@
 package main
 
 import (
+	// "log"
+	"html/template"
+
 	"golang-fave/engine/wrapper"
 )
 
+type TmplMenuItem struct {
+	Name string
+	Link string
+}
+
+type TmplData struct {
+	PathSysIcoFav       string
+	PathSysCssBootstrap string
+	PathSysJsJquery     string
+	PathSysJsPopper     string
+	PathSysJsBootstrap  string
+
+	MetaTitle       string
+	MetaKeywords    string
+	MetaDescription string
+	MenuItems       []TmplMenuItem
+	SomeHtml        template.HTML
+}
+
 func handleFrontEnd(e *wrapper.Wrapper) bool {
-	return false
+	tmpl, err := template.ParseFiles(
+		e.DirVhostHome+"/template"+"/index.html",
+		e.DirVhostHome+"/template"+"/header.html",
+		e.DirVhostHome+"/template"+"/footer.html",
+	)
+	if err != nil {
+		// log.Printf(err.Error())
+		return false
+	}
+
+	tmpl.Execute(*e.W, TmplData{
+		PathSysIcoFav:       e.R.URL.Scheme + "://" + e.R.Host + "/assets/sys/fave.ico",
+		PathSysCssBootstrap: e.R.URL.Scheme + "://" + e.R.Host + "/assets/sys/bootstrap.css",
+		PathSysJsJquery:     e.R.URL.Scheme + "://" + e.R.Host + "/assets/sys/jquery.js",
+		PathSysJsPopper:     e.R.URL.Scheme + "://" + e.R.Host + "/assets/sys/popper.js",
+		PathSysJsBootstrap:  e.R.URL.Scheme + "://" + e.R.Host + "/assets/sys/bootstrap.js",
+
+		MetaTitle:       "Meta Title",
+		MetaKeywords:    "Meta Keywords",
+		MetaDescription: "Meta Description",
+
+		MenuItems: []TmplMenuItem{
+			{Name: "Menu Item 1", Link: "/#1"},
+			{Name: "Menu Item 2", Link: "/#2"},
+		},
+		SomeHtml: template.HTML("<div class=\"some-class\">DIV</div>"),
+	})
+	return true
 }
