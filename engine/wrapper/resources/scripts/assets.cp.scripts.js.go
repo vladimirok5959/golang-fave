f9c2a93f624bfe6ea309612cb33f5d16291b350b
Vova Tkach <vladimirok5959@gmail.com>
2018-12-28 09:35:21 +0200

    Update assets.cp.scripts.js.go

--- engine/wrapper/resources/scripts/assets.cp.scripts.js.go
+++ engine/wrapper/resources/scripts/assets.cp.scripts.js.go
@@ -1,5 +1,33 @@
 package scripts
 
 var File_assets_cp_scripts_js = []byte(`
-console.log('Start');
+$(document).ready(function() {
+	$('form').each(function() {
+		$(this).submit(function(e) {
+			var form = $(this);
+			if($(form).hasClass('loading')) {
+				e.preventDefault();
+				return;
+			}
+			$(form).addClass('loading');
+
+			$.ajax({
+				type: "POST",
+				url: form.attr('action'),
+				data: form.serialize()
+			}).done(function(data) {
+				console.log('done');
+				console.log(data);
+			}).fail(function() {
+				console.log('fail');
+			}).always(function() {
+				$(form).removeClass('loading');
+				console.log('always');
+			});
+
+			console.log('1');
+			e.preventDefault();
+		});
+	});
+});
 `)
