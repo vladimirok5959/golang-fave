33304b4f6debf4fe6414d679b3098be730f7f049
Vova Tkach <vladimirok5959@gmail.com>
2018-12-29 03:21:10 +0200

    Add modal dialog

---
+++ engine/wrapper/resources/templates/block.modal.sysmsg.go
@@ -0,0 +1,3 @@
+package templates
+
+var BlockModalSysMsg = []byte(`<div class="modal fade" id="sys-modal-msg" tabindex="-1" role="dialog" aria-labelledby="sysModalMsgLabel" aria-hidden="true"><div class="modal-dialog" role="document"><div class="modal-content"><div class="modal-header"><h5 class="modal-title" id="sysModalMsgLabel"></h5><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button></div><div class="modal-body" id="sysModalMsgBody"></div><div class="modal-footer" id="sysModalMsgButtons"><button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button></div></div></div></div>`)
