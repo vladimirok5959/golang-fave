018fbebb00cb282470644536d5628b6941890ff0
Vova Tkach <vladimirok5959@gmail.com>
2018-12-30 08:41:57 +0200

    TODO

--- engine/backend/backend.go
+++ engine/backend/backend.go
@@ -19,7 +19,9 @@ func New(wrapper *wrapper.Wrapper, db *sql.DB) *Backend {
 }
 
 func (this *Backend) Run() bool {
+	// TODO:
 	// Check if any user exists
+	// If not - display form to create first user
 
 	// Login page
 	return this.wrapper.TmplBackEnd(templates.CpLogin, nil)
