d7d1921ecc1d2c73fa4ee109d4b108119e28cd10
Vova Tkach <vladimirok5959@gmail.com>
2018-12-30 07:38:16 +0200

    Code refactoring

--- engine/backend/backend.go
+++ engine/backend/backend.go
@@ -1,1 +1,25 @@
 package backend
+
+import (
+	"database/sql"
+
+	"golang-fave/engine/wrapper"
+
+	templates "golang-fave/engine/wrapper/resources/templates"
+)
+
+type Backend struct {
+	wrapper *wrapper.Wrapper
+	db      *sql.DB
+}
+
+func New(wrapper *wrapper.Wrapper, db *sql.DB) *Backend {
+	return &Backend{wrapper, db}
+}
+
+func (this *Backend) Run() bool {
+	// Check if any user exists
+
+	// Login page
+	return this.wrapper.TmplBackEnd(templates.CpLogin, nil)
+}
--- engine/frontend/frontend.go
+++ engine/frontend/frontend.go
@@ -1,1 +1,24 @@
 package frontend
+
+import (
+	"database/sql"
+
+	"golang-fave/engine/wrapper"
+)
+
+type Frontend struct {
+	wrapper *wrapper.Wrapper
+	db      *sql.DB
+}
+
+func New(wrapper *wrapper.Wrapper, db *sql.DB) *Frontend {
+	return &Frontend{wrapper, db}
+}
+
+func (this *Frontend) Run() bool {
+	if this.wrapper.R.URL.Path == "/" {
+		return this.wrapper.TmplFrontEnd("index", nil)
+	}
+
+	return false
+}
