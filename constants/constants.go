b75e25be94bdc278c7785ffdafd4e8244b32a6fc
Vova Tkach <vladimirok5959@gmail.com>
2018-12-31 10:18:44 +0200

    Revert "Update constants.go"
    This reverts commit 7edf618c90fd04070af933663ac5f46df424dbc5.

--- constants/constants.go
+++ constants/constants.go
@@ -3,9 +3,3 @@ package constants
 const Debug = !false
 const ServerVersion = "1.0.1"
 const AssetsVersion = "4"
-
-const DirNameVHostConfig = "config"
-const DirNameVHostStatic = "htdocs"
-const DirNameVHostLogs = "logs"
-const DirNameVHostTmpl = "template"
-const DirNameVHostTemp = "tmp"
