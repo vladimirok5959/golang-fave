cbf5a7c4ed3ee36927d9349a6101774694b2c9a3
Vova Tkach <vladimirok5959@gmail.com>
2019-02-23 14:08:15 +0200

    Pages mysql schema to structure

---
+++ utils/mysql_struct_pages.go
@@ -0,0 +1,14 @@
+package utils
+
+type MySql_page struct {
+	A_id               int
+	A_user             int
+	A_name             string
+	A_alias            string
+	A_content          string
+	A_meta_title       string
+	A_meta_keywords    string
+	A_meta_description string
+	A_datetime         int
+	A_status           int
+}
