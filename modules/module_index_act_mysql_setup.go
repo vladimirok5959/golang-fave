6222be1bea930778da5ee07d294f76c3f345d2a7
Vova Tkach <vladimirok5959@gmail.com>
2019-03-15 10:31:12 +0200

    Install action in one transaction

--- modules/module_index_act_mysql_setup.go
+++ modules/module_index_act_mysql_setup.go
@@ -4,7 +4,6 @@ import (
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
 
-	"fmt"
 	"os"
 	"strconv"
 
@@ -67,32 +66,90 @@ func (this *Modules) RegisterAction_IndexMysqlSetup() *Action {
 			return
 		}
 
-		// Try to install all tables
-		_, err = db.Query(fmt.Sprintf(
-			`CREATE TABLE %s.users (
+		// Start transaction
+		tx, err := db.Begin()
+		if err != nil {
+			wrap.MsgError(err.Error())
+			return
+		}
+
+		// Table: blog_cats
+		if _, err = tx.Exec(
+			`CREATE TABLE ` + pf_name + `.blog_cats (
 				id int(11) NOT NULL AUTO_INCREMENT COMMENT 'AI',
-				first_name VARCHAR(64) NOT NULL DEFAULT '' COMMENT 'User first name',
-				last_name VARCHAR(64) NOT NULL DEFAULT '' COMMENT 'User last name',
-				email VARCHAR(64) NOT NULL COMMENT 'User email',
-				password VARCHAR(32) NOT NULL COMMENT 'User password (MD5)',
-				admin int(1) NOT NULL COMMENT 'Is admin user or not',
-				active int(1) NOT NULL COMMENT 'Is active user or not',
+				user int(11) NOT NULL COMMENT 'User id',
+				name varchar(255) NOT NULL COMMENT 'Category name',
+				alias varchar(255) NOT NULL COMMENT 'Category alias',
+				lft int(11) NOT NULL COMMENT 'For nested set model',
+				rgt int(11) NOT NULL COMMENT 'For nested set model',
 				PRIMARY KEY (id)
-			) ENGINE = InnoDB;`,
-			pf_name))
-		if err != nil {
+			) ENGINE=InnoDB DEFAULT CHARSET=utf8;`,
+		); err != nil {
+			tx.Rollback()
 			wrap.MsgError(err.Error())
 			return
 		}
-		_, err = db.Query(fmt.Sprintf(
-			`ALTER TABLE %s.users ADD UNIQUE KEY email (email);`,
-			pf_name))
-		if err != nil {
+		if _, err = tx.Exec(`INSERT INTO ` + pf_name + `.blog_cats (id, user, name, alias, lft, rgt) VALUES (1, 0, 'ROOT', 'ROOT', 1, 2);`); err != nil {
+			tx.Rollback()
 			wrap.MsgError(err.Error())
 			return
 		}
-		_, err = db.Query(fmt.Sprintf(
-			`CREATE TABLE %s.pages (
+		if _, err = tx.Exec(`ALTER TABLE ` + pf_name + `.blog_cats ADD UNIQUE KEY alias (alias);`); err != nil {
+			tx.Rollback()
+			wrap.MsgError(err.Error())
+			return
+		}
+		if _, err = tx.Exec(`ALTER TABLE ` + pf_name + `.blog_cats ADD KEY lft (lft), ADD KEY rgt (rgt);`); err != nil {
+			tx.Rollback()
+			wrap.MsgError(err.Error())
+			return
+		}
+
+		// Table: blog_cat_post_rel
+		if _, err = tx.Exec(
+			`CREATE TABLE ` + pf_name + `.blog_cat_post_rel (
+				id int(11) NOT NULL AUTO_INCREMENT COMMENT 'AI',
+				post_id int(11) NOT NULL COMMENT 'Post id',
+				category_id int(11) NOT NULL COMMENT 'Category id',
+				PRIMARY KEY (id)
+			) ENGINE=InnoDB DEFAULT CHARSET=utf8;`,
+		); err != nil {
+			tx.Rollback()
+			wrap.MsgError(err.Error())
+			return
+		}
+		if _, err = tx.Exec(`ALTER TABLE blog_cat_post_rel ADD KEY post_id (post_id), ADD KEY category_id (category_id);`); err != nil {
+			tx.Rollback()
+			wrap.MsgError(err.Error())
+			return
+		}
+
+		// Table: blog_posts
+		if _, err = tx.Exec(
+			`CREATE TABLE ` + pf_name + `.blog_posts (
+				id int(11) NOT NULL AUTO_INCREMENT COMMENT 'AI',
+				user int(11) NOT NULL COMMENT 'User id',
+				name varchar(255) NOT NULL COMMENT 'Post name',
+				alias varchar(255) NOT NULL COMMENT 'Post alias',
+				content text NOT NULL COMMENT 'Post content',
+				datetime datetime NOT NULL COMMENT 'Creation date/time',
+				active int(1) NOT NULL COMMENT 'Is active post or not',
+				PRIMARY KEY (id)
+			) ENGINE=InnoDB DEFAULT CHARSET=utf8;`,
+		); err != nil {
+			tx.Rollback()
+			wrap.MsgError(err.Error())
+			return
+		}
+		if _, err = tx.Exec(`ALTER TABLE blog_posts ADD UNIQUE KEY alias (alias);`); err != nil {
+			tx.Rollback()
+			wrap.MsgError(err.Error())
+			return
+		}
+
+		// Table: pages
+		if _, err = tx.Exec(
+			`CREATE TABLE ` + pf_name + `.pages (
 				id int(11) NOT NULL AUTO_INCREMENT COMMENT 'AI',
 				user int(11) NOT NULL COMMENT 'User id',
 				name varchar(255) NOT NULL COMMENT 'Page name',
@@ -105,13 +162,13 @@ func (this *Modules) RegisterAction_IndexMysqlSetup() *Action {
 				active int(1) NOT NULL COMMENT 'Is active page or not',
 				PRIMARY KEY (id)
 			) ENGINE=InnoDB DEFAULT CHARSET=utf8;`,
-			pf_name))
-		if err != nil {
+		); err != nil {
+			tx.Rollback()
 			wrap.MsgError(err.Error())
 			return
 		}
-		_, err = wrap.DB.Query(
-			`INSERT INTO %s.pages SET
+		if _, err = tx.Exec(
+			`INSERT INTO `+pf_name+`.pages SET
 				id = ?,
 				user = ?,
 				name = ?,
@@ -120,7 +177,6 @@ func (this *Modules) RegisterAction_IndexMysqlSetup() *Action {
 				datetime = ?,
 				active = ?
 			;`,
-			pf_name,
 			1,
 			1,
 			"Home",
@@ -128,13 +184,13 @@ func (this *Modules) RegisterAction_IndexMysqlSetup() *Action {
 			"<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Feugiat in ante metus dictum at tempor commodo ullamcorper a. Et malesuada fames ac turpis egestas sed tempus urna et. Euismod elementum nisi quis eleifend. Nisi porta lorem mollis aliquam ut porttitor. Ac turpis egestas maecenas pharetra convallis posuere. Nunc non blandit massa enim nec dui. Commodo elit at imperdiet dui accumsan sit amet nulla. Viverra accumsan in nisl nisi scelerisque. Dui nunc mattis enim ut tellus. Molestie ac feugiat sed lectus vestibulum mattis ullamcorper. Faucibus ornare suspendisse sed nisi lacus. Nulla facilisi morbi tempus iaculis. Ut eu sem integer vitae justo eget magna fermentum iaculis. Ullamcorper sit amet risus nullam eget felis eget nunc. Volutpat sed cras ornare arcu dui vivamus. Eget magna fermentum iaculis eu non diam.</p><p>Arcu ac tortor dignissim convallis aenean et tortor. Vitae auctor eu augue ut lectus arcu. Ac turpis egestas integer eget aliquet nibh praesent. Interdum velit euismod in pellentesque massa placerat duis. Vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras tincidunt. Nisl rhoncus mattis rhoncus urna neque viverra justo. Odio ut enim blandit volutpat. Ac auctor augue mauris augue neque gravida. Ut lectus arcu bibendum at varius vel. Porttitor leo a diam sollicitudin tempor id eu nisl nunc. Dolor sit amet consectetur adipiscing elit duis tristique. Semper quis lectus nulla at volutpat diam ut. Sapien eget mi proin sed.</p><p>Ante metus dictum at tempor commodo ullamcorper a. Facilisis mauris sit amet massa vitae. Enim neque volutpat ac tincidunt vitae. Tempus quam pellentesque nec nam aliquam sem. Mollis aliquam ut porttitor leo a diam sollicitudin. Nunc pulvinar sapien et ligula ullamcorper. Dignissim suspendisse in est ante in nibh mauris. Eget egestas purus viverra accumsan in. Vitae tempus quam pellentesque nec nam aliquam sem et. Sodales ut etiam sit amet nisl. Aliquet risus feugiat in ante. Rhoncus urna neque viverra justo nec ultrices dui sapien. Sit amet aliquam id diam maecenas ultricies. Sed odio morbi quis commodo odio aenean sed adipiscing diam.</p>",
 			utils.UnixTimestampToMySqlDateTime(utils.GetCurrentUnixTimestamp()),
 			1,
-		)
-		if err != nil {
+		); err != nil {
+			tx.Rollback()
 			wrap.MsgError(err.Error())
 			return
 		}
-		_, err = wrap.DB.Query(
-			`INSERT INTO %s.pages SET
+		if _, err = tx.Exec(
+			`INSERT INTO `+pf_name+`.pages SET
 				id = ?,
 				user = ?,
 				name = ?,
@@ -143,7 +199,6 @@ func (this *Modules) RegisterAction_IndexMysqlSetup() *Action {
 				datetime = ?,
 				active = ?
 			;`,
-			pf_name,
 			2,
 			1,
 			"Another",
@@ -151,13 +206,13 @@ func (this *Modules) RegisterAction_IndexMysqlSetup() *Action {
 			"<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Feugiat in ante metus dictum at tempor commodo ullamcorper a. Et malesuada fames ac turpis egestas sed tempus urna et. Euismod elementum nisi quis eleifend. Nisi porta lorem mollis aliquam ut porttitor. Ac turpis egestas maecenas pharetra convallis posuere. Nunc non blandit massa enim nec dui. Commodo elit at imperdiet dui accumsan sit amet nulla. Viverra accumsan in nisl nisi scelerisque. Dui nunc mattis enim ut tellus. Molestie ac feugiat sed lectus vestibulum mattis ullamcorper. Faucibus ornare suspendisse sed nisi lacus. Nulla facilisi morbi tempus iaculis. Ut eu sem integer vitae justo eget magna fermentum iaculis. Ullamcorper sit amet risus nullam eget felis eget nunc. Volutpat sed cras ornare arcu dui vivamus. Eget magna fermentum iaculis eu non diam.</p><p>Arcu ac tortor dignissim convallis aenean et tortor. Vitae auctor eu augue ut lectus arcu. Ac turpis egestas integer eget aliquet nibh praesent. Interdum velit euismod in pellentesque massa placerat duis. Vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras tincidunt. Nisl rhoncus mattis rhoncus urna neque viverra justo. Odio ut enim blandit volutpat. Ac auctor augue mauris augue neque gravida. Ut lectus arcu bibendum at varius vel. Porttitor leo a diam sollicitudin tempor id eu nisl nunc. Dolor sit amet consectetur adipiscing elit duis tristique. Semper quis lectus nulla at volutpat diam ut. Sapien eget mi proin sed.</p><p>Ante metus dictum at tempor commodo ullamcorper a. Facilisis mauris sit amet massa vitae. Enim neque volutpat ac tincidunt vitae. Tempus quam pellentesque nec nam aliquam sem. Mollis aliquam ut porttitor leo a diam sollicitudin. Nunc pulvinar sapien et ligula ullamcorper. Dignissim suspendisse in est ante in nibh mauris. Eget egestas purus viverra accumsan in. Vitae tempus quam pellentesque nec nam aliquam sem et. Sodales ut etiam sit amet nisl. Aliquet risus feugiat in ante. Rhoncus urna neque viverra justo nec ultrices dui sapien. Sit amet aliquam id diam maecenas ultricies. Sed odio morbi quis commodo odio aenean sed adipiscing diam.</p>",
 			utils.UnixTimestampToMySqlDateTime(utils.GetCurrentUnixTimestamp()),
 			1,
-		)
-		if err != nil {
+		); err != nil {
+			tx.Rollback()
 			wrap.MsgError(err.Error())
 			return
 		}
-		_, err = wrap.DB.Query(
-			`INSERT INTO %s.pages SET
+		if _, err = tx.Exec(
+			`INSERT INTO `+pf_name+`.pages SET
 				id = ?,
 				user = ?,
 				name = ?,
@@ -166,7 +221,6 @@ func (this *Modules) RegisterAction_IndexMysqlSetup() *Action {
 				datetime = ?,
 				active = ?
 			;`,
-			pf_name,
 			3,
 			1,
 			"About",
@@ -174,73 +228,42 @@ func (this *Modules) RegisterAction_IndexMysqlSetup() *Action {
 			"<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Feugiat in ante metus dictum at tempor commodo ullamcorper a. Et malesuada fames ac turpis egestas sed tempus urna et. Euismod elementum nisi quis eleifend. Nisi porta lorem mollis aliquam ut porttitor. Ac turpis egestas maecenas pharetra convallis posuere. Nunc non blandit massa enim nec dui. Commodo elit at imperdiet dui accumsan sit amet nulla. Viverra accumsan in nisl nisi scelerisque. Dui nunc mattis enim ut tellus. Molestie ac feugiat sed lectus vestibulum mattis ullamcorper. Faucibus ornare suspendisse sed nisi lacus. Nulla facilisi morbi tempus iaculis. Ut eu sem integer vitae justo eget magna fermentum iaculis. Ullamcorper sit amet risus nullam eget felis eget nunc. Volutpat sed cras ornare arcu dui vivamus. Eget magna fermentum iaculis eu non diam.</p><p>Arcu ac tortor dignissim convallis aenean et tortor. Vitae auctor eu augue ut lectus arcu. Ac turpis egestas integer eget aliquet nibh praesent. Interdum velit euismod in pellentesque massa placerat duis. Vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras tincidunt. Nisl rhoncus mattis rhoncus urna neque viverra justo. Odio ut enim blandit volutpat. Ac auctor augue mauris augue neque gravida. Ut lectus arcu bibendum at varius vel. Porttitor leo a diam sollicitudin tempor id eu nisl nunc. Dolor sit amet consectetur adipiscing elit duis tristique. Semper quis lectus nulla at volutpat diam ut. Sapien eget mi proin sed.</p><p>Ante metus dictum at tempor commodo ullamcorper a. Facilisis mauris sit amet massa vitae. Enim neque volutpat ac tincidunt vitae. Tempus quam pellentesque nec nam aliquam sem. Mollis aliquam ut porttitor leo a diam sollicitudin. Nunc pulvinar sapien et ligula ullamcorper. Dignissim suspendisse in est ante in nibh mauris. Eget egestas purus viverra accumsan in. Vitae tempus quam pellentesque nec nam aliquam sem et. Sodales ut etiam sit amet nisl. Aliquet risus feugiat in ante. Rhoncus urna neque viverra justo nec ultrices dui sapien. Sit amet aliquam id diam maecenas ultricies. Sed odio morbi quis commodo odio aenean sed adipiscing diam.</p>",
 			utils.UnixTimestampToMySqlDateTime(utils.GetCurrentUnixTimestamp()),
 			1,
-		)
-		if err != nil {
+		); err != nil {
+			tx.Rollback()
 			wrap.MsgError(err.Error())
 			return
 		}
-		_, err = db.Query(fmt.Sprintf(
-			`ALTER TABLE %s.pages ADD UNIQUE KEY alias (alias);`,
-			pf_name))
-		if err != nil {
+		if _, err = tx.Exec(`ALTER TABLE pages ADD UNIQUE KEY alias (alias);`); err != nil {
+			tx.Rollback()
 			wrap.MsgError(err.Error())
 			return
 		}
-		_, err = db.Query(fmt.Sprintf(
-			`CREATE TABLE %s.blog_posts (
+
+		// Table: users
+		if _, err = tx.Exec(
+			`CREATE TABLE ` + pf_name + `.users (
 				id int(11) NOT NULL AUTO_INCREMENT COMMENT 'AI',
-				user int(11) NOT NULL COMMENT 'User id',
-				name varchar(255) NOT NULL COMMENT 'Post name',
-				alias varchar(255) NOT NULL COMMENT 'Post alias',
-				content text NOT NULL COMMENT 'Post content',
-				datetime datetime NOT NULL COMMENT 'Creation date/time',
-				active int(1) NOT NULL COMMENT 'Is active post or not',
+				first_name varchar(64) NOT NULL DEFAULT '' COMMENT 'User first name',
+				last_name varchar(64) NOT NULL DEFAULT '' COMMENT 'User last name',
+				email varchar(64) NOT NULL COMMENT 'User email',
+				password varchar(32) NOT NULL COMMENT 'User password (MD5)',
+				admin int(1) NOT NULL COMMENT 'Is admin user or not',
+				active int(1) NOT NULL COMMENT 'Is active user or not',
 				PRIMARY KEY (id)
 			) ENGINE=InnoDB DEFAULT CHARSET=utf8;`,
-			pf_name))
-		if err != nil {
+		); err != nil {
+			tx.Rollback()
 			wrap.MsgError(err.Error())
 			return
 		}
-		_, err = db.Query(fmt.Sprintf(
-			`ALTER TABLE %s.blog_posts ADD UNIQUE KEY alias (alias);`,
-			pf_name))
-		if err != nil {
+		if _, err = tx.Exec(`ALTER TABLE users ADD UNIQUE KEY email (email);`); err != nil {
+			tx.Rollback()
 			wrap.MsgError(err.Error())
 			return
 		}
-		_, err = db.Query(fmt.Sprintf(
-			`CREATE TABLE %s.blog_cats (
-				id int(11) NOT NULL AUTO_INCREMENT COMMENT 'AI',
-				user int(11) NOT NULL COMMENT 'User id',
-				name varchar(255) NOT NULL COMMENT 'Category name',
-				alias varchar(255) NOT NULL COMMENT 'Category alias',
-				lft int(11) NOT NULL COMMENT 'For nested set model',
-				rgt int(11) NOT NULL COMMENT 'For nested set model',
-				PRIMARY KEY (id)
-			) ENGINE=InnoDB DEFAULT CHARSET=utf8;`,
-			pf_name))
-		if err != nil {
-			wrap.MsgError(err.Error())
-			return
-		}
-		_, err = db.Query(fmt.Sprintf(
-			`ALTER TABLE %s.blog_cats ADD UNIQUE KEY alias (alias);`,
-			pf_name))
-		if err != nil {
-			wrap.MsgError(err.Error())
-			return
-		}
-		_, err = db.Query(fmt.Sprintf(
-			`INSERT INTO %s.blog_cats (id, user, name, alias, lft, rgt) VALUES (1, 0, 'ROOT', 'ROOT', 1, 2);`,
-			pf_name))
-		if err != nil {
-			wrap.MsgError(err.Error())
-			return
-		}
-		_, err = db.Query(fmt.Sprintf(
-			`ALTER TABLE %s.blog_cats ADD KEY lft (lft), ADD KEY rgt (rgt);`,
-			pf_name))
+
+		// Commit all changes
+		err = tx.Commit()
 		if err != nil {
 			wrap.MsgError(err.Error())
 			return
