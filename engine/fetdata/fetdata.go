939162381e2ff062b48427d1fca7bb79bba267f3
Vova Tkach <vladimirok5959@gmail.com>
2019-03-27 22:48:13 +0200

    Start rework front-end

--- engine/fetdata/fetdata.go
+++ engine/fetdata/fetdata.go
@@ -36,6 +36,18 @@ func (this *FERData) init() *FERData {
 	return this
 }
 
+func (this *FERData) RequestURI() string {
+	return this.wrap.R.RequestURI
+}
+
+func (this *FERData) RequestURL() string {
+	return this.wrap.R.URL.Path
+}
+
+func (this *FERData) RequestGET() string {
+	return utils.ExtractGetParams(this.wrap.R.RequestURI)
+}
+
 func (this *FERData) Module() string {
 	if this.is404 {
 		return "404"
