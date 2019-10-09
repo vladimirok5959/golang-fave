52d080e80c41ea6a13583f5b616e725dd772b5e3
Vova Tkach <vladimirok5959@gmail.com>
2019-10-09 06:41:48 +0300

    Add sending test message to SMTP save settings action

--- modules/module_settings_act_smtp.go
+++ modules/module_settings_act_smtp.go
@@ -17,6 +17,7 @@ func (this *Modules) RegisterAction_SettingsSmtp() *Action {
 		pf_smtp_port := strings.TrimSpace(wrap.R.FormValue("smtp-port"))
 		pf_smtp_login := strings.TrimSpace(wrap.R.FormValue("smtp-login"))
 		pf_smtp_password := strings.TrimSpace(wrap.R.FormValue("smtp-password"))
+		pf_smtp_test_email := strings.TrimSpace(wrap.R.FormValue("smtp-test-email"))
 
 		(*wrap.Config).SMTP.Host = pf_smtp_host
 		(*wrap.Config).SMTP.Port = utils.StrToInt(pf_smtp_port)
@@ -30,6 +31,18 @@ func (this *Modules) RegisterAction_SettingsSmtp() *Action {
 		if err := wrap.ConfigSave(); err != nil {
 			wrap.MsgError(err.Error())
 			return
+		}
+
+		// Send test message
+		if pf_smtp_test_email != "" {
+			if err := wrap.SendEmail(
+				pf_smtp_test_email,
+				"Fave.Pro SMTP test message",
+				"Hello! This is Fave.Pro test message.<br />If you see this message, then you right configured SMTP settings!",
+			); err != nil {
+				wrap.MsgError(err.Error())
+				return
+			}
 		}
 
 		// Reload current page
