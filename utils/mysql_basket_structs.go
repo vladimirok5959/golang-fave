c9cfb255806f9a621010950478201d6f6fccde1e
Vova Tkach <vladimirok5959@gmail.com>
2019-11-26 00:08:21 +0200

    Cleanup

--- utils/mysql_basket_structs.go
+++ utils/mysql_basket_structs.go
@@ -20,19 +20,3 @@ type MySql_basket struct {
 	TotalSum   float64
 	TotalCount int
 }
-
-// type MySql_basket_order struct {
-// 	A_currency_id int
-// 	A_currency_name string
-// 	A_currency_coefficient float64
-// 	A_currency_code string
-// 	A_currency_symbol string
-// 	A_client_last_name string
-// 	A_client_first_name string
-// 	A_client_second_name string
-// 	A_client_phone string
-// 	A_client_email string
-// 	A_client_delivery_comment string
-// 	A_client_order_comment string
-// 	A_status string
-// }
