a5eeb724abad01b690c9a1b880afec0a94e9e9b7
Vova Tkach <vladimirok5959@gmail.com>
2019-12-16 01:11:08 +0200

    Update mysql_struct_shop_product.go

--- utils/mysql_struct_shop_product.go
+++ utils/mysql_struct_shop_product.go
@@ -5,24 +5,25 @@ import (
 )
 
 type MySql_shop_product struct {
-	A_id        int
-	A_parent    sql.NullInt64
-	A_user      int
-	A_currency  int
-	A_price     float64
-	A_price_old float64
-	A_gname     string
-	A_name      string
-	A_alias     string
-	A_vendor    string
-	A_quantity  int
-	A_category  int
-	A_briefly   string
-	A_content   string
-	A_datetime  int
-	A_active    int
-	A_custom1   string
-	A_custom2   string
+	A_id          int
+	A_parent      sql.NullInt64
+	A_user        int
+	A_currency    int
+	A_price       float64
+	A_price_old   float64
+	A_price_promo float64
+	A_gname       string
+	A_name        string
+	A_alias       string
+	A_vendor      string
+	A_quantity    int
+	A_category    int
+	A_briefly     string
+	A_content     string
+	A_datetime    int
+	A_active      int
+	A_custom1     string
+	A_custom2     string
 }
 
 func (this *MySql_shop_product) A_parent_id() int {
