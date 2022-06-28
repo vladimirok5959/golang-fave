a64cf7557a66c44ee3af823a405471347fbe87c0
Volodymyr Tkach <vladimirok5959@gmail.com>
2022-06-28 12:55:48 +0300

    Upgrade Cypress to latest version, migrate follow recommendations

---
+++ support/cypress.config.js
@@ -0,0 +1,14 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  projectId: 'hv532c',
+  video: true,
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+  },
+})
